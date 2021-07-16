1372dcf8f5612ef32c0951c4b18bf4a38a34738c
Rodrigo Engelberg <=>
2021-07-16 01:43:34 -0300

    Atualizações Migration

--- src/shared/infra/typeorm/migrations/1601469847641-CreateMembers.ts
+++ src/shared/infra/typeorm/migrations/1601469847641-CreateMembers.ts
@@ -15,13 +15,37 @@ export default class CreateAppointments1601469847641
             default: 'uuid_generate_v4()',
           },
           {
+            name: 'first_name',
+            type: 'varchar',
+          },
+          {
+            name: 'last_name',
+            type: 'varchar',
+          },
+          {
             name: 'email',
             type: 'varchar',
             isUnique: true
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'gender',
+            type: 'varchar',
+          },
+          {
+            name: 'member_type',
+            type: 'varchar',
+          },
+          {
+            name: 'marital_status',
+            type: 'varchar',
+          },
+          {
+            name: 'nationality',
+            type: 'varchar',
+          },
+          {
+            name: 'birth_date',
+            type: 'date',
           },
           {
             name: 'created_at',
