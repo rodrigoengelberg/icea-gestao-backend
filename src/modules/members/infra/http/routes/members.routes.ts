2dbb8e6425ff2b001841e743c49eb8d06369e709
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-08-12 01:28:29 -0300

    Add tests to service to MemberSpiritual

--- src/modules/members/infra/http/routes/members.routes.ts
+++ src/modules/members/infra/http/routes/members.routes.ts
@@ -3,7 +3,6 @@ import { Router } from 'express'
 import MembersController from '../controllers/MembersController'
 import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
-
 const membersRouter = Router()
 
 const membersController = new MembersController()
@@ -16,20 +15,27 @@ membersRouter.post(
   celebrate({
     [Segments.BODY]: {
       first_name: Joi.string().required(),
-      last_name: Joi.string().required(),
-      email: Joi.string().email().required(),
+      full_name: Joi.string().required(),
+      email: Joi.string().email(),
       gender: Joi.string().required(),
-      member_type: Joi.string().required(),
-      marital_status: Joi.string().required(),
+      marital_status: Joi.string(),
       nationality: Joi.string().required(),
-      birth_date: Joi.date().iso().required(),
+      birth_date: Joi.date().iso(),
       member_contact: {
         address: Joi.string().required(),
-        state: Joi.string().required(),
-        city: Joi.string().required(),
-        zipcode: Joi.number().required(),
-        phoneType: Joi.string().required(),
-        phoneNumber: Joi.number().required()
+        state: Joi.string(),
+        city: Joi.string(),
+        zipcode: Joi.number(),
+        phone_type: Joi.number(),
+        phone_type_name: Joi.string(),
+        phone_number: Joi.number()
+      },
+      member_details: {
+        avatar: Joi.string(),
+        occupation: Joi.string(),
+        schooling: Joi.string(),
+        facebook_link: Joi.string(),
+        instagram_link: Joi.string()
       }
     },
   }),
@@ -42,20 +48,27 @@ membersRouter.put(
     [Segments.BODY]: {
       id: Joi.string().optional(),
       first_name: Joi.string().required(),
-      last_name: Joi.string().required(),
-      email: Joi.string().email().required(),
+      full_name: Joi.string().required(),
+      email: Joi.string().email(),
       gender: Joi.string().required(),
-      member_type: Joi.string().required(),
-      marital_status: Joi.string().required(),
+      marital_status: Joi.string(),
       nationality: Joi.string().required(),
-      birth_date: Joi.date().iso().required(),
+      birth_date: Joi.date().iso(),
       member_contact: {
         address: Joi.string().required(),
-        state: Joi.string().required(),
-        city: Joi.string().required(),
-        zipcode: Joi.number().required(),
-        phoneType: Joi.string().required(),
-        phoneNumber: Joi.number().required()
+        state: Joi.string(),
+        city: Joi.string(),
+        zipcode: Joi.number(),
+        phone_type: Joi.number(),
+        phone_type_name: Joi.string(),
+        phone_number: Joi.number()
+      },
+      member_details: {
+        avatar: Joi.string(),
+        occupation: Joi.string(),
+        schooling: Joi.string(),
+        facebook_link: Joi.string(),
+        instagram_link: Joi.string()
       },
       created_at: Joi.string().optional(),
       updated_at: Joi.string().optional()
