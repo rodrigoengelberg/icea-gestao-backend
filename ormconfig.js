5ef1e235fe1c3fdf80152bf48acc142ed6c73383
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-10-20 22:54:37 -0300

    Ajusts to Heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,26 +1,28 @@
 module.exports = [
   {
-    "name": "default",
-    "type": "postgres",
-    "url": process.env.DATABASE_URL,
+    name: 'default',
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    // postgres://xlmiyfsxcayqzi:5c0c419bfb39d8f002d2f513c76ec94b67882e48ca716a090c91b13064955b30@ec2-54-208-17-82.compute-1.amazonaws.com:5432/d2r36ecrlgs719
     // "host": "localhost",
     // "port": 5432,
     // "username": "docker",
     // "password": "docker",
     // "database": "gostack_gobarber",
-    "entities": [
-      "./src/modules/**/infra/typeorm/entities/*.ts"
+    entities: [
+      './' + process.env.LOCAL_DIR + '/modules/**/infra/typeorm/entities/*.ts'
       //"./dist/modules/**/infra/typeorm/entities/*.js"
     ],
-    "migrations": [
-      "./src/shared/infra/typeorm/migrations/*.ts"
+    migrations: [
+      './' + process.env.LOCAL_DIR + '/shared/infra/typeorm/migrations/*.ts'
       //"./dist/shared/infra/typeorm/migrations/*.js"
     ],
-    "cli": {
-      "migrationsDir": "./src/shared/infra/typeorm/migrations"
+    cli: {
+      migrationsDir:
+        './' + process.env.LOCAL_DIR + '/shared/infra/typeorm/migrations'
       //"migrationsDir": "./dist/shared/infra/typeorm/migrations"
     }
-  },
+  }
   // {
   //   "name": "mongo",
   //   "type": "mongodb",
