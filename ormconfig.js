13648c86eca49c53f9299b494e6c54faa3a6a0bc
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-10-22 18:44:06 -0300

    Ajusts to Heroku

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ module.exports = [
     name: 'default',
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_SSL === `true`,
+    ssl: true,
     // postgres://xlmiyfsxcayqzi:5c0c419bfb39d8f002d2f513c76ec94b67882e48ca716a090c91b13064955b30@ec2-54-208-17-82.compute-1.amazonaws.com:5432/d2r36ecrlgs719
     // "host": "localhost",
     // "port": 5432,
