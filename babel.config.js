4f882eb3fd5e54ec189a895b68a80113eb2be48d
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-10-22 19:25:05 -0300

    Ajusts to Heroku

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript'
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@shared': './src/shared'
+        }
+      }
     ],
-    plugins: [
-        ['module-resolver', {
-            alias: {
-                "@modules": "./src/modules",
-                "@config": "./src/config",
-                "@shared": "./src/shared"
-            }
-        }],
-        'babel-plugin-transform-typescript-metadata',
-        ['@babel/plugin-proposal-decorators', { 'legacy': true }]
-    ],
-    ignore: [
-        '**/*.spec.ts'
-    ]
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
+  ],
+  ignore: ['**/*.spec.ts']
 }
