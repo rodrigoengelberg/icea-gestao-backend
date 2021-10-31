8fa1e0b726ba704523c4a4aab70bcf56c36badba
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-10-30 21:18:35 -0300

    Ajusts to Heroku

--- src/shared/infra/http/middleware/rateLimiter.ts
+++ src/shared/infra/http/middleware/rateLimiter.ts
@@ -4,23 +4,33 @@ import redis from 'redis'
 
 import AppError from '@shared/errors/AppError'
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS || undefined,
-})
+let redisClient: any = {}
+
+if (!process.env.REDIS_TLS_URL) {
+  redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASS || undefined
+  })
+} else {
+  redisClient = redis.createClient(process.env.REDIS_TLS_URL, {
+    tls: {
+      rejectUnauthorized: false
+    }
+  })
+}
 
 const limiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: 'ratelimit',
   points: 5,
-  duration: 1,
+  duration: 1
 })
 
 export default async function rateLimiter(
   request: Request,
   response: Response,
-  next: NextFunction,
+  next: NextFunction
 ): Promise<void> {
   try {
     await limiter.consume(request.ip)
