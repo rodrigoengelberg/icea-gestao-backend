95a54b7c0da0f092619ea676b2158594e6fd7fd4
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2021-11-03 15:13:52 -0300

    Ajusts to Heroku

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,17 +1,16 @@
-import { injectable, inject } from 'tsyringe';
+import { injectable, inject } from 'tsyringe'
 
-import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError'
 
-import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
-import User from '../infra/typeorm/entities/User';
-import IUsersRepository from '../repositories/IUsersRepository';
-import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import User from '../infra/typeorm/entities/User'
+import IUsersRepository from '../repositories/IUsersRepository'
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
 
 interface IRequest {
-  name: string;
-  email: string;
-  password: string;
-  role?: string;
+  name: string
+  email: string
+  password: string
+  role?: string
 }
 
 @injectable()
@@ -21,37 +20,34 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
 
     @inject('HashProvider')
-    private hashProvider: IHashProvider,
-
-    @inject('CacheProvider')
-    private cacheProvider: ICacheProvider,
+    private hashProvider: IHashProvider
   ) {}
 
   public async execute({
     name,
     email,
     password,
-    role,
+    role
   }: IRequest): Promise<User> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    const checkUserExists = await this.usersRepository.findByEmail(email)
 
     if (checkUserExists) {
-      throw new AppError('Email address already used.');
+      throw new AppError('Email address already used.')
     }
 
-    const hashedPassword = await this.hashProvider.generateHash(password);
+    const hashedPassword = await this.hashProvider.generateHash(password)
 
     const user = await this.usersRepository.create({
       name,
       email,
       password: hashedPassword,
-      role,
-    });
+      role
+    })
 
-    await this.cacheProvider.invalidatePrefix('providers-list');
+    //await this.cacheProvider.invalidatePrefix('providers-list');
 
-    return user;
+    return user
   }
 }
 
-export default CreateUserService;
+export default CreateUserService
