fbd6ae21c9d212e7b6b85e9e8f537aff291e8c30
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2022-01-11 00:05:47 -0300

    Creating Migration Table Finances

--- ormconfig.js
+++ ormconfig.js
@@ -3,9 +3,9 @@ module.exports = [
     name: 'default',
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    // ssl: {
-    //   rejectUnauthorized: false
-    // },
+    ssl: {
+      rejectUnauthorized: false
+    },
     // postgres://xlmiyfsxcayqzi:5c0c419bfb39d8f002d2f513c76ec94b67882e48ca716a090c91b13064955b30@ec2-54-208-17-82.compute-1.amazonaws.com:5432/d2r36ecrlgs719
     // "host": "localhost",
     // "port": 5432,
@@ -13,17 +13,17 @@ module.exports = [
     // "password": "docker",
     // "database": "gostack_gobarber",
     entities: [
-      './src/modules/**/infra/typeorm/entities/*.ts'
-      // './dist/modules/**/infra/typeorm/entities/*.js'
+      // './src/modules/**/infra/typeorm/entities/*.ts'
+      './dist/modules/**/infra/typeorm/entities/*.js'
     ],
     migrations: [
-      './src/shared/infra/typeorm/migrations/*.ts'
-      // './dist/shared/infra/typeorm/migrations/*.js'
+      // './src/shared/infra/typeorm/migrations/*.ts'
+      './dist/shared/infra/typeorm/migrations/*.js'
     ],
     cli: {
       migrationsDir:
-        './src/shared/infra/typeorm/migrations'
-        // './dist/shared/infra/typeorm/migrations'
+        // './src/shared/infra/typeorm/migrations'
+        './dist/shared/infra/typeorm/migrations'
     }
   }
   // {
