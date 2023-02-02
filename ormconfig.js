8daf05f7a7b353b371dab41eb34423955958c8e9
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2023-02-02 18:42:48 -0300

    Ajuste para resolver erro de compilação

--- ormconfig.js
+++ ormconfig.js
@@ -7,23 +7,23 @@ module.exports = [
       rejectUnauthorized: false
     },
     // postgres://xlmiyfsxcayqzi:5c0c419bfb39d8f002d2f513c76ec94b67882e48ca716a090c91b13064955b30@ec2-54-208-17-82.compute-1.amazonaws.com:5432/d2r36ecrlgs719
-    // "host": "localhost",
-    // "port": 5432,
-    // "username": "docker",
-    // "password": "docker",
-    // "database": "gostack_gobarber",
+     "host": "localhost",
+     "port": 5432,
+     "username": "postgres",
+     "password": "1234",
+     "database": "postgres",
     entities: [
-      './src/modules/**/infra/typeorm/entities/*.ts'
-      // './dist/modules/**/infra/typeorm/entities/*.js'
+      //  './src/modules/**/infra/typeorm/entities/*.ts'
+      './dist/modules/**/infra/typeorm/entities/*.js'
     ],
     migrations: [
-      './src/shared/infra/typeorm/migrations/*.ts'
-      // './dist/shared/infra/typeorm/migrations/*.js'
+      //  './src/shared/infra/typeorm/migrations/*.ts'
+      './dist/shared/infra/typeorm/migrations/*.js'
     ],
     cli: {
       migrationsDir:
-        './src/shared/infra/typeorm/migrations'
-        // './dist/shared/infra/typeorm/migrations'
+        //  './src/shared/infra/typeorm/migrations'
+        './dist/shared/infra/typeorm/migrations'
     }
   }
   // {
