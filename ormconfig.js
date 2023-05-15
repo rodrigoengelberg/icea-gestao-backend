b9462bad31c098653d191c4464036679b11f730b
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2023-05-15 19:26:03 -0300

    Ajustes para DB com Docker

--- ormconfig.js
+++ ormconfig.js
@@ -2,16 +2,16 @@ module.exports = [
   {
     name: 'default',
     type: 'postgres',
-    url: process.env.DATABASE_URL,
+    // url: process.env.DATABASE_URL,
     // ssl: {
     //   rejectUnauthorized: true
     // },
     // postgres://xlmiyfsxcayqzi:5c0c419bfb39d8f002d2f513c76ec94b67882e48ca716a090c91b13064955b30@ec2-54-208-17-82.compute-1.amazonaws.com:5432/d2r36ecrlgs719
-    // host: "localhost",
-    // port: 5432,
-    // username: "admicea",
-    // password: "1234",
-    // database: "postgres",
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [
       //  './src/modules/**/infra/typeorm/entities/*.ts'
       './dist/modules/**/infra/typeorm/entities/*.js'
