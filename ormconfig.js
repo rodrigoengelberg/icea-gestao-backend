a427849bc422ade5e65cd7a594ffd4994defdbcf
Rodrigo Engelberg <rodrigoengelberg@gmail.com>
2023-05-18 18:19:49 -0300

    Ajustes para correção do database no cloud

--- ormconfig.js
+++ ormconfig.js
@@ -2,15 +2,15 @@ module.exports = [
   {
     name: 'default',
     type: 'postgres',
-    url: process.env.DATABASE_URL,
+    // url: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: process.env.SSL_REJECT
     },
-    // host: process.env.DB_SERVER,
-    // port: process.env.DB_PORT,
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [
       //  './src/modules/**/infra/typeorm/entities/*.ts'
       './dist/modules/**/infra/typeorm/entities/*.js'
